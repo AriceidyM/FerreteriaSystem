e5ef03292530cc2052b10b8c12d3e1d4a408a5bb
Rosy <Rosy@DESKTOP-HI3GI4E>
2019-07-09 20:32:04 -0400

    DAL listo con su nombre llamado: FerreteriaContexto

---
+++ DAL/FerreteriaContexto.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class FerreteriaContexto : DbContext
+    {
+        public DbSet<Usuarios> usuario { get; set; }
+        public FerreteriaContexto() : base("ConStr")
+        { }
+    }
+}
