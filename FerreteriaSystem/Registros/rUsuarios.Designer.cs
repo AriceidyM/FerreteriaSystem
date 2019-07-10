882ce80ed684549f6b5cacaed8eae0a8c2159620
Rosy <Rosy@DESKTOP-HI3GI4E>
2019-07-09 21:25:12 -0400

    Dise;o de la ventana de registro de usuarios

---
+++ FerreteriaSystem/Registros/rUsuarios.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace FerreteriaSystem.Registros
+{
+    partial class rUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FechaIngresodateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.UsuarioIDnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.ClavetextBox = new System.Windows.Forms.TextBox();
+            this.UsuariotextBox = new System.Windows.Forms.TextBox();
+            this.NivelUsuariotextBox = new System.Windows.Forms.TextBox();
+            this.EmailstextBox = new System.Windows.Forms.TextBox();
+            this.NombrestextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuarioIDnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FechaIngresodateTimePicker
+            // 
+            this.FechaIngresodateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.FechaIngresodateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechaIngresodateTimePicker.Location = new System.Drawing.Point(109, 222);
+            this.FechaIngresodateTimePicker.Name = "FechaIngresodateTimePicker";
+            this.FechaIngresodateTimePicker.Size = new System.Drawing.Size(100, 20);
+            this.FechaIngresodateTimePicker.TabIndex = 29;
+            // 
+            // UsuarioIDnumericUpDown
+            // 
+            this.UsuarioIDnumericUpDown.Location = new System.Drawing.Point(108, 15);
+            this.UsuarioIDnumericUpDown.Name = "UsuarioIDnumericUpDown";
+            this.UsuarioIDnumericUpDown.Size = new System.Drawing.Size(61, 20);
+            this.UsuarioIDnumericUpDown.TabIndex = 28;
+            // 
+            // ClavetextBox
+            // 
+            this.ClavetextBox.Location = new System.Drawing.Point(109, 192);
+            this.ClavetextBox.Name = "ClavetextBox";
+            this.ClavetextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClavetextBox.TabIndex = 27;
+            // 
+            // UsuariotextBox
+            // 
+            this.UsuariotextBox.Location = new System.Drawing.Point(109, 161);
+            this.UsuariotextBox.Name = "UsuariotextBox";
+            this.UsuariotextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsuariotextBox.TabIndex = 26;
+            // 
+            // NivelUsuariotextBox
+            // 
+            this.NivelUsuariotextBox.Location = new System.Drawing.Point(109, 126);
+            this.NivelUsuariotextBox.Name = "NivelUsuariotextBox";
+            this.NivelUsuariotextBox.Size = new System.Drawing.Size(100, 20);
+            this.NivelUsuariotextBox.TabIndex = 25;
+            // 
+            // EmailstextBox
+            // 
+            this.EmailstextBox.Location = new System.Drawing.Point(109, 92);
+            this.EmailstextBox.Name = "EmailstextBox";
+            this.EmailstextBox.Size = new System.Drawing.Size(100, 20);
+            this.EmailstextBox.TabIndex = 24;
+            // 
+            // NombrestextBox
+            // 
+            this.NombrestextBox.Location = new System.Drawing.Point(109, 53);
+            this.NombrestextBox.Name = "NombrestextBox";
+            this.NombrestextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombrestextBox.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Fecha de ingreso";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Clave";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Nivel de Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Emails";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Nombres";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "UsuarioID";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Image = global::FerreteriaSystem.Properties.Resources.search_black_icon;
+            this.Buscarbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Buscarbutton.Location = new System.Drawing.Point(198, 6);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 34);
+            this.Buscarbutton.TabIndex = 33;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Image = global::FerreteriaSystem.Properties.Resources.delete_file_icon;
+            this.Eliminarbutton.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Eliminarbutton.Location = new System.Drawing.Point(180, 275);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(78, 34);
+            this.Eliminarbutton.TabIndex = 32;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Image = global::FerreteriaSystem.Properties.Resources.Save_icon;
+            this.Guardarbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Guardarbutton.Location = new System.Drawing.Point(97, 275);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(77, 34);
+            this.Guardarbutton.TabIndex = 31;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Image = global::FerreteriaSystem.Properties.Resources.Actions_contact_new_icon;
+            this.Nuevobutton.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Nuevobutton.Location = new System.Drawing.Point(12, 275);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(79, 34);
+            this.Nuevobutton.TabIndex = 30;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            // 
+            // rUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 326);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.FechaIngresodateTimePicker);
+            this.Controls.Add(this.UsuarioIDnumericUpDown);
+            this.Controls.Add(this.ClavetextBox);
+            this.Controls.Add(this.UsuariotextBox);
+            this.Controls.Add(this.NivelUsuariotextBox);
+            this.Controls.Add(this.EmailstextBox);
+            this.Controls.Add(this.NombrestextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "rUsuarios";
+            this.Text = "rUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.UsuarioIDnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.DateTimePicker FechaIngresodateTimePicker;
+        private System.Windows.Forms.NumericUpDown UsuarioIDnumericUpDown;
+        private System.Windows.Forms.TextBox ClavetextBox;
+        private System.Windows.Forms.TextBox UsuariotextBox;
+        private System.Windows.Forms.TextBox NivelUsuariotextBox;
+        private System.Windows.Forms.TextBox EmailstextBox;
+        private System.Windows.Forms.TextBox NombrestextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
